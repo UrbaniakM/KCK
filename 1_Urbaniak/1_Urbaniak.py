ddb63c9d48a94bb24d3f631f798a33184c85e60d
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-10-05 22:33:35 +0200

    Finished

--- 1_Urbaniak/1_Urbaniak.py
+++ 1_Urbaniak/1_Urbaniak.py
@@ -17,7 +17,8 @@ def main():
     markers = ['8', 'v', 'D', 's', 'd']
     colors = ['b', 'g', 'r', 'k', '#ee82ee']
     boxPlotData = []
-    plt.figure(figsize=(7, 6))
+    fig = plt.figure(figsize=(7, 6))
+    fig.set_size_inches(6.7, 6.7)
     scattPlot = plt.subplot('121')
     boxPlot = plt.subplot('122')
     fontSize = 8
@@ -37,12 +38,17 @@ def main():
             gamesPercentWon.append(suma*100/(len(row) - 2))
         scattPlot.plot(gamesPlayed, gamesPercentWon, label = seriesNames[i], marker = markers[i], markevery=25, color = colors[i], markeredgecolor='k', linewidth = 1)
         boxPlotData.append(finalResult)
-    boxPlot.boxplot(boxPlotData, notch=True, sym='+')
+    meanProps = dict(marker='o', markeredgecolor='k', markerfacecolor='b', markersize=4)
+    boxPlotEl = boxPlot.boxplot(boxPlotData, notch=True, sym='b+', showmeans=True, meanprops=meanProps)
     boxPlot.yaxis.tick_right()
     boxPlot.set_ylim(60,100)
     boxPlot.tick_params('both',direction = 'in', labelsize = fontSize)
     boxPlot.grid(color='#D3D3D3', linestyle='dotted', linewidth=1)
-    boxPlot.set_xticklabels(seriesNames, rotation=45)
+    boxPlot.set_xticklabels(seriesNames, rotation=20)
+    for element in ['boxes', 'fliers', 'means','caps']:
+        plt.setp(boxPlotEl[element], color='b')
+    plt.setp(boxPlotEl['whiskers'], color = 'b', linestyle='--', dashes=(5, 5))
+    plt.setp(boxPlotEl['medians'],color='r')
     scattPlot.legend()
     scattPlot.tick_params('both',direction = 'in', labelsize = fontSize)
     scattPlot.set_xticks([x*100 for x in range(0, 6)])
@@ -57,6 +63,9 @@ def main():
     topAxis.set_xticks([x*40 for x in range(0, 6)])
     topAxis.set_xlabel('Pokolenie', fontsize = fontSize)
     topAxis.tick_params('both',direction = 'in', labelsize = fontSize)
+    gridLines = boxPlot.get_xgridlines() + boxPlot.get_ygridlines() + scattPlot.get_xgridlines() + scattPlot.get_ygridlines()
+    for line in gridLines:
+        line.set_dashes((1,4))
     plt.savefig('1_Urbaniak.pdf')
     plt.close()
         
