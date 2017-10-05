b8318248a59e1a633a2b579fd9cb631ebf2a920d
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-10-05 13:04:18 +0200

    Added plot, using matplotlib.pyplot

--- 1_Urbaniak/1_Urbaniak.py
+++ 1_Urbaniak/1_Urbaniak.py
@@ -3,7 +3,7 @@
 import urllib.request
 import codecs
 import csv
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 def main():
     urlStream = [ \
@@ -12,12 +12,26 @@ def main():
         urllib.request.urlopen("http://www.cs.put.poznan.pl/wjaskowski/pub/teaching/kck/labs/python/ipd-choices-9-005/cel-rs.csv"), \
         urllib.request.urlopen("http://www.cs.put.poznan.pl/wjaskowski/pub/teaching/kck/labs/python/ipd-choices-9-005/cel.csv"), \
         urllib.request.urlopen("http://www.cs.put.poznan.pl/wjaskowski/pub/teaching/kck/labs/python/ipd-choices-9-005/rsel.csv")]
-    series = ["1-Evol-RS", "1-Coev-RS", "2-Coev-RS", "1-Coev", "2-Coev"]
-    for i in range(len(urlPath)):
+    seriesNames = ["1-Evol-RS", "1-Coev-RS", "2-Coev-RS", "1-Coev", "2-Coev"]
+    plt.figure(figsize=(3, 3))
+    for i in range(len(urlStream)):
         csvReader = csv.reader(codecs.iterdecode(urlStream[i], 'utf-8'))
-        pokolenie = []
+        generation = []
+        gamesPlayed = [] #x1000
+        gamesPercentWon = []
+        next(csvReader, None)
         for row in csvReader:
-            pokolenie.append(row[0])
+            generation.append(int(row[0]))
+            gamesPlayed.append(float(row[1])/1000)
+            suma = 0.0
+            for col in range(2,len(row)):
+                suma += float(row[col])
+            gamesPercentWon.append(suma*100/(len(row) - 2))
+        plt.plot(gamesPlayed, gamesPercentWon, label = seriesNames[i])
+    plt.legend()
+    plt.savefig('1_Urbaniak.pdf')
+    plt.close()
+        
 
 if __name__ == '__main__':
         main()
