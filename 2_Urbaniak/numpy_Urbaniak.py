0eb8870f2bbdcc45d8280080278f354ccdd7d85b
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-10-16 22:19:52 +0200

    Finished

--- 2_Urbaniak/numpy_Urbaniak.py
+++ 2_Urbaniak/numpy_Urbaniak.py
@@ -1,6 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def draw(xi, vi, n):
+  sinPlot = plt.subplot(3, 1, n+1)
+  sinPlot.plot(xi, vi, label='y=sin(x)')
+  sinPlot.set_xlim(-np.pi, np.pi)
+  sinPlot.set_xlabel('x')
+  sinPlot.set_ylabel('y')
+  sinPlot.grid(True)
+  sinPlot.tick_params('both', direction='in')
+  sinPlot.grid(color='#D3D3D3', linestyle='dotted', linewidth=1)
+  sinPlot.legend()
+  sinPlot.set_title('y=sin(x), −π<=x<=π')
+
 basicMatrix = np.array([[ 1, 3, 1, 2],
 [ 1, 2, 5, 8],
 [ 3, 1, 2, 9],
@@ -14,22 +26,13 @@ transponedMatrix = np.array([[ 2, 3, 1],
 
 dotMatrix = cuttedMatrix.dot(transponedMatrix)
 
-x = [np.linspace(-np.pi,np.pi,n.pi), np.linspace(-np.pi,np.pi,np.pi/10), np.linspace(-np.pi,np.pi,np.pi/100)]  # STEPS
-v = [np.sin(x)] # albo bedzie v = [np.sin(i) for i in range(len(x))] 
 
-for i, x_i, y_i in range(len(x)),x,v:
-  fig = plt.figure(figsize=(3,3))
-  fig.set_size_inches(6.7,6.5)
-  sinPlot = plt.subplot(3,1,1+i)
-  sinPlot.plot(x_i,v_i, label='y=sin(x)')
-  sinPlot.set_xlim(-np.pi,np.pi)
-  sinPlot.set_xlabel('x')
-  sinPlot.set_ylabel('y')
-  sinPlot.grid(True)
-  sinPlot.tick_params('both',direction = 'in')
-  sinPlot.grid(color='#D3D3D3', linestyle='dotted', linewidth=1)
-  sinPlot.legend()
-  sinPlot.set_title('y=sin(x), −π<=x<=π')
+x = [np.linspace(-np.pi,np.pi,np.pi), np.linspace(-np.pi,np.pi,10), np.linspace(-np.pi,np.pi,100)]  # STEPS
+v = [np.sin(i) for i in x]
+label = ['krok pi', 'krok 10', 'krok 100']
+
+for i in range(len(x)):
+  draw(x[i],v[i],i)
 plt.savefig('sinusPlot_Urbaniak.pdf')
 plt.close()
 
